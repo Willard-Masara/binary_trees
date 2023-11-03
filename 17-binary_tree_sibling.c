0b284667ba0bd2c98908cc3cc9e17de3c089c9a0
Willard-Masara <willardmasarat@gmail.com>
2023-11-02 17:36:47 -0700

    recursive aspect of if is hidden

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,15 +9,10 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
-	return (NULL);
-
-	binary_tree_t *parent = node->parent;
-
-	if (parent->left == node)
-	return (parent->right);
-	else if (parent->right == node)
-	return (parent->left);
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
 	else
-	return (NULL);
+		return (node->parent->left);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,11 +7,20 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+size_t l = 0;
+size_t r = 0;
+
+if (tree == NULL)
+{
 	return (0);
-
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height ? left_height : right_height) + 1);
+}
+else
+{
+if (tree)
+{
+	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+}
+return ((l > r) ? l : r);
+}
 }
